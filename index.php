17f0defde88bcc23130f0ffc8a782c94f9bf70aa
Gabriele Pagnotta <110975140+GabrielePagnotta@users.noreply.github.com>
2022-12-12 15:57:17 +0100

    fine esercizio

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$parola1="***";
+$parola2=$_GET[parola];
+$frase="Da grande farò il $parola1";
+$frase2="Da grande farò il $parola2";
+$lunghezza1=strlen($frase);
+$lunghezza2=strlen($frase2);
+
+
+echo $frase2;
+echo "<br>";
+echo "il testo è lungo: $lunghezza2 caratteri";
+
+echo "<br>";
+echo "<br>";
+
+echo $frase;
+echo "<br>";
+echo "il testo è lungo: $lunghezza1 caratteri";
+
+
